feb5a45a912f8769528bda5513757d7d7c87353c
Dr. Arun Kumar Pandey <arunp77@gmail.com>
2024-08-31 09:49:58 +0200

    Create run_doctests.py

---
+++ run_doctests.py
@@ -0,0 +1,20 @@
+# run_doctests.py
+#====================================================================================
+#
+# To run this script, you need to uncomment below and then run it. 
+# Run the script using `python run_doctests.py` and it will test the arthmetic module.
+#
+#=====================================================================================
+
+# import doctest
+# import mathlib.arithmetic
+# # import other modules as needed
+
+# def run_doctests():
+#     """Run doctests for the mathlib package."""
+#     doctest.testmod(mathlib.arithmetic)
+#     # add other modules here
+#     print("Doctests completed.")
+
+# if __name__ == "__main__":
+#     run_doctests()
