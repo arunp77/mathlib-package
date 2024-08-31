a311af24f64fada67684a3f37827f73942e06e5d
Dr. Arun Kumar Pandey <arunp77@gmail.com>
2024-08-31 09:43:58 +0200

    Update test_arithmetic.py

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -61,3 +61,11 @@ def test_power():
 # if __name__ == '__main__':
 #     unittest.main()
 
+#----------------- doctest ------------------
+# # mathlib/arithmetic.py
+
+# # ... [existing code] ...
+
+# if __name__ == "__main__":
+#     import doctest
+#     doctest.testmod()
