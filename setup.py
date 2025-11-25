9e1c9e40ac4221cc6a58eb87428e5511baac4440
Dr. Arun Kumar Pandey <arunp77@gmail.com>
2025-11-25 14:47:42 +0100

    setup file updated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url='https://github.com/arunp77/mathlib-package/',
     license='MIT',
     install_requires=[],
-    # test_suite='tests',  # Specify the test suite
+    # test_suite='tests',  
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
